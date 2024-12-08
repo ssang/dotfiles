f225c9fd8e785821adcf88c26b55afc0e5ba256a
Robert Lee <rslee1247@gmail.com>
2024-12-07 17:22:10 -0800

    Add treesitter

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -200,3 +200,18 @@ use({
         vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
     end
 })
+
+-- Improved syntax highlighting
+use({
+  'nvim-treesitter/nvim-treesitter',
+  run = function()
+    require('nvim-treesitter.install').update({ with_sync = true })
+  end,
+  requires = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  config = function()
+    require('user/plugins/treesitter')
+  end,
+})
